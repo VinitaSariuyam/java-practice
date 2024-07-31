b5148215bc43fcffb2ea0bda0f2f0e69c03c73ca
vinitaSariuyam <vinitasariyam937@gmail.com>
2024-07-31 20:46:06 +0530

    first commit

---
+++ five.java
@@ -0,0 +1,14 @@
+public class five {
+public static void main(String[]args) {
+
+    double a = 57.34;
+    int i = (int)a;
+
+    System.out.println(i);
+    char b ='A';
+    float j = (char)b;
+    System.out.println(j);
+    
+
+  }   
+}
