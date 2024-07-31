f7cd00ad608bd87800ce5158a52bdd406e3c4681
vinitaSariuyam <vinitasariyam937@gmail.com>
2024-07-31 20:17:29 +0530

    initial commit

---
+++ third.java
@@ -0,0 +1,24 @@
+public class third {
+    public static void main(String[]args){
+
+        int a = 50;
+        short b = 45;
+        long c = 780592166;
+        byte d = 122;
+        char first = 'A';
+        float e = 33.12f;
+        double f = 564.56;
+        boolean g = true;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(first);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        
+    }
+    
+}
