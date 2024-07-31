7c858aeea708c76e1d7854c979a9fb478b7755d4
vinitaSariuyam <vinitasariyam937@gmail.com>
2024-07-31 20:35:34 +0530

    initial commit

---
+++ four.java
@@ -0,0 +1,25 @@
+public class four {
+    public static void main(String[]args){
+
+        Object a = 50;
+
+        Object first = 'A';
+
+        Object b = 34.8f;
+
+
+        Object c = 7805926;
+
+        Object d = true;
+
+        System.out.println(a.getClass().getSimpleName());
+        System.out.println(first.getClass().getSimpleName());
+        System.out.println(b.getClass().getSimpleName());
+        System.out.println(c.getClass().getSimpleName());
+        System.out.println(d.getClass().getSimpleName());
+        
+
+    }
+
+    
+}
