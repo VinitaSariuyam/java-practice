907345343751913f87118601fead7f33a2314b68
vinitaSariuyam <vinitasariyam937@gmail.com>
2024-07-31 22:11:50 +0530

    initial commit

---
+++ eight.java
@@ -0,0 +1,34 @@
+public class eight {
+public static void main(String[] args) {
+    String name ="aman";
+
+    //charAt
+    System.out.println(name.charAt(0));
+
+    //length
+    System.out.println(name.length());
+
+    //replace
+    String name2 = name.replace('a','b');
+
+    System.out.println(name2);
+   
+//substring
+    String name3 ="aman and akku";
+    System.out.println(name.substring(0,4));
+//concatenate
+    String name4 = name + " and " + name3;
+    System.out.println(name4);
+
+
+
+
+
+
+
+
+    
+
+
+}    
+}
