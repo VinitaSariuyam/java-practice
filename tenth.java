bebabc49c0440bb0597a4bdb8814a1a2b9981943
vinitaSariuyam <vinitasariyam937@gmail.com>
2024-08-03 17:25:43 +0530

    initial commit

---
+++ tenth.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+
+public class tenth {
+    public static void main (String []args){
+        Scanner Sc = new Scanner (System.in);
+
+        int n = Sc.nextInt();
+
+        for( int i=1; i<11;i++){
+            i= i+1;
+
+            System.out.println(n*i);
+
+        }
+
+
+    }
+    
+}
