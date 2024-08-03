b7a03ea8b16f454ea5aa6b9e4c6072dd2c442055
vinitaSariuyam <vinitasariyam937@gmail.com>
2024-08-03 17:30:34 +0530

    initial commit

---
+++ eleventh.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+
+public class eleventh {
+    public static void main (String[]args){
+
+        Scanner Sc = new Scanner(System.in);
+        int n = Sc.nextInt();
+
+        int sum =0;
+        for( int i =0; i<=4;i++){
+            sum=sum+i;
+
+
+        }
+        System.out.println(sum);
+
+    }
+    
+}
