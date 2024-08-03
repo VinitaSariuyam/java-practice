1b267f950d9f058db73de344c0a92e12f7b355a8
vinitaSariuyam <vinitasariyam937@gmail.com>
2024-08-03 17:39:08 +0530

    initial commit

---
+++ twelve.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+
+public class twelve {
+    public static void main (String[]args){
+
+        Scanner Sc = new Scanner(System.in);
+
+        int a = Sc.nextInt();
+        int b= Sc.nextInt();
+
+        if(a==b){
+            System.out.println("equal");
+        }
+        else if( a>b){
+            System.out.println("a is greater");
+
+        }
+        else{
+            System.out.println("a is lesser");
+            
+        }
+
+    }
+    
+}
