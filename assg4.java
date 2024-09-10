8b951fff08ccdf667f616d06d7fbc7a2c45b83c1
VinitaSariuyam <Vinitasariyam937@gmail.com>
2024-09-10 21:15:40 +0530

    initial commit

---
+++ assg4.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+
+
+public class assg4 {
+
+    public static void main(String[]args){
+
+        Scanner Sc = new Scanner(System.in);
+
+        int a = Sc.nextInt();
+        int b = Sc.nextInt();
+
+
+        int temp =a;
+        a=b;
+        b= temp;
+
+        System.out.println(" a "+ a);
+        System.out.println(" b "+ b);
+
+             
+        }
+    }
+    
+
