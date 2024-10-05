dd8a9302777b5fa167ef8bbb67bde13a2f97d4dd
VinitaSariuyam <Vinitasariyam937@gmail.com>
2024-10-05 20:26:13 +0530

    initial commit

---
+++ assg5.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+
+public class assg5 {
+
+  
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter distance in miles: ");
+        double miles = scanner.nextDouble();
+
+        double kilometers = miles * 1.60934;
+        System.out.println(miles + " miles is equal to " + kilometers + " kilometers");
+        
+        scanner.close();
+    }
+}
+
+    
+
